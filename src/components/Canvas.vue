059e5d3de9be957af786f85e53687635dc325a63
Steven P. Kennedy <umbrylax@gmail.com>
2021-02-02 16:33:40 -0700

    Setting up the canvas

---
+++ src/components/Canvas.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="Canvas">
+    <canvas> </canvas>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Canvas",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {},
+};
+</script>
+
+
+<style scoped>
+</style>
